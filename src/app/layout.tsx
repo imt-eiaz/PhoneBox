9412175b0ba00fd6f11d630726797951f68e4647
Imtiaz Khan <imteiaz@gmail.com>
2024-11-29 10:42:54 +0000

    Page Title Changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import Provider from "@/layouts/Provider";
 
 export const metadata = {
-    title: {
-        default: "PhoneCareHub",
-        template: "%s - PhoneCareHub",
-    },
+  title: {
+    default: "PhoneBox",
+    template: "%s - PhoneCareHub",
+  },
 };
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
-    return (
-        <html lang="en">
-            <body>
-                <Provider>{children}</Provider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
 }
