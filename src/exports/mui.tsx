99ad5e3b4aa789421cdc84595c926fc57542a545
Imtiaz Khan <imteiaz@gmail.com>
2024-11-29 17:43:35 +0000

    Footer Changed

--- src/exports/mui.tsx
+++ src/exports/mui.tsx
@@ -5,19 +5,46 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import useStateStore from "@/hooks/zustand/useStateStore";
 import NextLink, { type LinkProps as NextLinkProps } from "next/link";
-import { Container, Stack, Typography, Divider, Collapse, Grid, Link as MuiLink, LinkProps, Box, Avatar, Rating } from "@mui/material";
+import {
+  Container,
+  Stack,
+  Typography,
+  Divider,
+  Collapse,
+  Grid,
+  Link as MuiLink,
+  LinkProps,
+  Box,
+  Avatar,
+  Rating,
+} from "@mui/material";
 
 export function Link(props: LinkProps & NextLinkProps) {
-    return <MuiLink component={NextLink} {...props} />;
+  return <MuiLink component={NextLink} {...props} />;
 }
 
 export function MobileMenu({ children }: React.PropsWithChildren) {
-    const { toggleMenu } = useStateStore();
-    return <Collapse in={toggleMenu}>{children}</Collapse>;
+  const { toggleMenu } = useStateStore();
+  return <Collapse in={toggleMenu}>{children}</Collapse>;
 }
 
 export function Copyright() {
-    return <Typography align="center">&copy; {new Date().getFullYear()} PhoneCareHub, All Rights Reserved.</Typography>;
+  return (
+    <Typography align="center">
+      &copy; {new Date().getFullYear()} Phone Box, All Rights Reserved.
+    </Typography>
+  );
 }
 
-export { Container, Stack, Typography, Divider, Grid, Box, Carousel, Avatar, Rating, CountUp };
+export {
+  Container,
+  Stack,
+  Typography,
+  Divider,
+  Grid,
+  Box,
+  Carousel,
+  Avatar,
+  Rating,
+  CountUp,
+};
